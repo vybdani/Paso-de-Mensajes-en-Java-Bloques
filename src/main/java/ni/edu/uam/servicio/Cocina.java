25f36225a5660ae0aa8f5c3cde92dfffcc072496
vybdani <darochap@uamv.edu.ni>
2026-05-06 16:20:17 -0600

    Clase Cocina

--- src/main/java/ni/edu/uam/servicio/Cocina.java
+++ src/main/java/ni/edu/uam/servicio/Cocina.java
@@ -1,1 +1,25 @@
+package ni.edu.uam.servicio;
 
+import ni.edu.uam.modelos.Pedido;
+import javax.swing.JOptionPane;
+
+public class Cocina {
+
+    public void prepararPedido(Pedido pedido) {
+        JOptionPane.showMessageDialog(null,
+                "Cocina recibió el pedido",
+                "Dani's Sweet Café · Cocina", JOptionPane.PLAIN_MESSAGE);
+
+        pedido.cambiarEstado("En preparación");
+
+        JOptionPane.showMessageDialog(null,
+                "Estado: En preparación\nPreparando tu pedido...",
+                "Dani's Sweet Café · Cocina", JOptionPane.PLAIN_MESSAGE);
+
+        pedido.cambiarEstado("Listo");
+
+        JOptionPane.showMessageDialog(null,
+                "Estado: Listo\n¡Pedido completado y listo para entregar!",
+                "Dani's Sweet Café · Cocina", JOptionPane.PLAIN_MESSAGE);
+    }
+}
